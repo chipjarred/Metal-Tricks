c1770d4ae2758afc7c399531a428982922bf3d90
Chip <chipjarred@gmail.com>
2019-12-06 05:03:22 -0600

    Initial commit

---
+++ Metal-Tricks/Metal-Tricks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Metal-Tricks
+//
+//  Created by Chip Jarred on 12/6/19.
+//  Copyright © 2019 Chip Jarred. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
